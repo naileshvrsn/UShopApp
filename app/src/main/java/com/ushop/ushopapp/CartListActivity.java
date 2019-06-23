43a78a1190e75bcf90e686639d681bb21e8e49fa
Shyam Vekaria <svekaria124@gmail.com>
2019-06-23 21:45:05 +1200

    cartlist progress

--- app/src/main/java/com/ushop/ushopapp/CartListActivity.java
+++ app/src/main/java/com/ushop/ushopapp/CartListActivity.java
@@ -1,22 +1,22 @@
 package com.ushop.ushopapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.TextView;
 
- com.firebase.ui.database.FirebaseRecyclerOptions;
-import com.firebase.ui.database.FirebaseRecyclerOptions;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.CollectionReference;
-import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
-import com.google.firebase.firestore.Query;
-import com.google.firebase.firestore.model.DocumentCollections;
+
 
 public class CartListActivity extends AppCompatActivity {
 
@@ -60,12 +60,6 @@ public class CartListActivity extends AppCompatActivity {
     protected void onStart() {
         super.onStart();
 
-        // db reference
-        currentUser = mAuth.getCurrentUser();
-        cartListRef = db.collection("cartList").document(currentUser.getUid()).collection("products");
-
-
-        FirebaseRecyclerOptions<Cart> options = new FirebaseRecyclerOptions.Builder<Cart>().setQuery(Cart.class).build();
 
 
     }
