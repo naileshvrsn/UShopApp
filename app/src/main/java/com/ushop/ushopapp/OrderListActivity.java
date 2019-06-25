a24cb4390c341dc61461b96096db2e88277c768c
Shyam Vekaria <svekaria124@gmail.com>
2019-06-25 15:41:03 +1200

    OrderList

---
+++ app/src/main/java/com/ushop/ushopapp/OrderListActivity.java
@@ -0,0 +1,21 @@
+package com.ushop.ushopapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class OrderListActivity extends AppCompatActivity {
+
+    private static final String TAG = "ConfirmOrderActivity";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_order_list);
+    }
+
+
+
+
+
+}
